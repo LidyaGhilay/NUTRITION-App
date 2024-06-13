a6664855e09a7a71b97d6015ef7bf08ed5eb8c96
lidyaghilay <lidya.ghilay@student.moringaschool.com>
2024-06-13 14:21:34 +0300

    commits

--- models/User.py
+++ models/User.py
@@ -1,5 +1,9 @@
 from models.Food import Food
 from models.Nutritionist import Nutritionist
+import sqlite3
+
+def get_db_connection():
+    return sqlite3.connect('database.db')
 
 class User:
     tableName="userDetails"
@@ -10,10 +14,62 @@ class User:
         self.age = age
     
     def save(self):
-        sql="f"
-           INSERT INTO {self.tableName} (name,phone)
-           VALUES (?,?)
+        sql=f"""
+I           INSERT INTO {self.tableName} (id, name, phone_number, age)
+           
         """
-        curso.execute(sql,(self.name,self.phone))
-        conn.commit
+        cursor.execute(sql,(self.name,self.phone))
+        conn.commit()
+        self.id = cursor.lastrowid
+        
+        return self
+    
+    def dictionary(self):
+        return{
+            "id": self.id,
+            "name":self.name,
+            "phone_number":self.age,
+            "age":self.age
+        }
+    
+
+
+    @classmethod
+    def using_the_phone(cls, phone):
+        sql = f"""
+            SELECT * FROM {cls.TABLE_NAME}
+            WHERE phone = ?
+        """
+
+        row = cursor.execute(sql, (phone,)).fetchone()
+
+        return cls.row_to_instance(row)
+
+    @classmethod
+    def row_to_instance(cls, row):
+        if row == None:
+            return None
+
+        user = cls(row[1], row[2])
+        user.id = row[0]
+
+        return user
+
+    @classmethod
+    def create_table(cls):
+      sql =f"""
+            CREATE TABLE IF NOT EXISTS{cls.TABLE_NAME}(
+              id INTERGER PRIMARY KEY AUTOINCREMENT,
+              name TEXT NOT NULL,
+              phone_number TEXT NOT NULL,
+              age TEXT NOT NULL,
+              )
+               
+                
+                 
+                  
+                   
+                    
+                     
+                       
 
